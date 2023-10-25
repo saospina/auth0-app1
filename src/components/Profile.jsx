51d366045e060294c40a9879d3d38f905d5f2b01
Sergio Ospina <s.ospina@fraktalsoftware.com>
2023-10-25 12:53:56 -0500

    auth0 app2 redirect

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,7 +2,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 const Profile = () => {
     const { user, isAuthenticated } = useAuth0()
     const handleRedirect = () => {
-        window.location.assign('http://localhost:5174/')
+        window.location.assign('http://saospina.github.io/auth0-app2')
     }
     return (
         isAuthenticated && (
