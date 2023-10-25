2426af7c690a9825eb0eb1e0348eee8e0ebad996
Sergio Ospina <s.ospina@fraktalsoftware.com>
2023-10-25 18:26:35 -0500

    auth0 app1 redirect

--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Auth0Provider
       domain="dev-eue8jfz2cney8cap.us.auth0.com"
       clientId="oT9T1Hr6oFEfm3HasIldXpX6ooCGAdWQ"
-      authorizationParams={{
-        redirect_uri: window.location.origin
-      }}
+      redirect_uri= "https://saospina.github.io/auth0-app1/"
     >
       <App />
     </Auth0Provider>
